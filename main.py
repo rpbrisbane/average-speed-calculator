b76fc9f52cabdfb7bbce1b458dc592432f6536a5
patrick <patrickdlacey@gmaillcom>
2024-03-22 22:21:27 -0700

    Add calculate speed method

--- main.py
+++ main.py
@@ -18,16 +18,16 @@ class SpeedCalculator(QWidget):
         self.time_line_edit = QLineEdit()
 
         self.metric_dropbox = QComboBox()
-        self.metric_dropbox['value'] = ('Metric (km)', 'Imperial (miles)')
+        self.metric_dropbox.addItems(['Metric (km)', 'Imperial (miles)'])
 
         calculate_button = QPushButton("Calculate Age")
-        # calculate_button.clicked.connect(self.calculate_speed())
+        calculate_button.clicked.connect(self.calculate_speed())
         self.output_label = QLabel("")
 
         # Add widgets to grid
         grid.addWidget(distance_label, 0, 0)
         grid.addWidget(self.distance_line_edit, 0, 1)
-        grid.addWidget(metric_dropbox, 0, 2)
+        grid.addWidget(self.metric_dropbox, 0, 2)
         grid.addWidget(time_label, 1, 0)
         grid.addWidget(self.time_line_edit, 1, 1)
         grid.addWidget(calculate_button, 2, 1)
@@ -36,8 +36,18 @@ class SpeedCalculator(QWidget):
         self.setLayout(grid)
 
     def calculate_speed(self):
-        distance_edit_input = self.distance_line_edit
-        time = self.time_line_edit
+        try:
+            # Convert user input to float
+            distance_edit_input = float(self.distance_line_edit.text())
+            time = float(self.time_line_edit.text())
+
+            # Calculate average speed
+            speed = distance_edit_input / time
+
+            # Display result
+            self.output_label.setText(f"Average Speed: {speed}")
+        except ValueError:
+            self.output_label.setText("Invalid input.")
 
 
 app = QApplication(sys.argv)
